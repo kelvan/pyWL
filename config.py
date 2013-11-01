6d8040c0f0c187ac4e8c896d61e7ee342b4e27d2
Florian Schweikert <kelvan@ist-total.org>
2013-11-01 21:34:43 +0100

    init

---
+++ config.py
@@ -0,0 +1,6 @@
+from key import senderid_dev
+
+baseurl = "http://www.wienerlinien.at/ogd_realtime"
+senderid= senderid_dev
+
+datetimeformat = '%Y-%m-%dT%H:%M:%S.%f%z'
---
+++ realtime.py
@@ -0,0 +1,73 @@
+import json
+import requests
+import logging
+from datetime import datetime
+
+import config
+
+logger = logging.getLogger("realtime_api")
+
+apiurl = "%s/monitor?sender=%s" % (config.baseurl, config.senderid)
+
+disruption_choices = {'short': 'stoerungkurz', 'long': 'stoerunglang',
+                      'elevator': 'aufzugsinfo'}
+traf_option = "activateTrafficInfo"
+position_option = "rbl"
+
+
+class Departures(dict):
+    def __init__(self, positions, disruptions=None):
+        self.disruptions = []
+        self.last_updated = None
+
+        if isinstance(positions, int):
+            self.postitions = [positions]
+        else:
+            self.postitions = positions
+
+        if isinstance(disruptions, str):
+            disruptions = [disruptions]
+        if not disruptions is None:
+            for disruption in disruptions:
+                if disruption in disruption_choices:
+                    self.disruptions.append(disruption)
+                else:
+                    logger.error("invalid disruption type: %s", disruption)
+
+    def refresh(self):
+        request_url = apiurl
+
+        for position in self.postitions:
+            request_url += "&%s=%d" % (position_option, position)
+
+        for disruption in self.disruptions:
+            request_url += "&%s=%s" % (traf_option,
+                                       disruption_choices[disruption])
+
+        logger.debug("processing %s", request_url)
+        r = requests.get(request_url)
+
+        if not r.status_code == 200:
+            logger.error('unable to fetch data, statuscode: %d', r.status_code)
+
+        j = json.loads(r.text)
+
+        self.last_status = j['message']['value']
+        if not self.last_status == 'OK':
+            logger.error('error server status: %s', self.last_status)
+        self.last_updated = datetime.strptime(j['message']['serverTime'],
+                                              config.datetimeformat)
+
+        for stop in j['data']['monitors']:
+            prop = stop['locationStop']['properties']
+            stopid = prop['attributes']['rbl']
+            self[stopid] = {'name': prop['title'], 'type': prop['type']}
+            self[stopid]['departures'] = []
+#
+#            for line in stop['lines']:
+#                for dep in line['departures']['departure']:
+#                    print(dep)
+#                    self[stopid]['departures'].append(dep['departure'])
+
+        # just for testing
+        return j
