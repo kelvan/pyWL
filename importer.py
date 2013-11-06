92a8285c5cfd68867dd6b387a168f1ef0cb604b8
Florian Schweikert <kelvan@ist-total.org>
2013-11-06 21:20:15 +0100

    importer rewrite
    lines, stations and communes; points missing

--- importer.py
+++ importer.py
@@ -1,9 +1,15 @@
-from sqlalchemy import *
-from sqlalchemy.orm import sessionmaker
+import csv
 from datetime import datetime
 
-from database import *
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+
 import stations
+from database import *
+
+line_file = 'wienerlinien-ogd-linien.csv'
+point_file = 'wienerlinien-ogd-steige.csv'
+station_file = 'wienerlinien-ogd-haltestellen.csv'
 
 engine = create_engine('sqlite:///wl.db', echo=False)
 
@@ -12,25 +18,78 @@ metadata.create_all(engine)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+session.autocommit = True
 
 dt_format = "%Y-%m-%d %H:%M:%S"
 
-for line in stations.Lines().values():
-    last_changed = datetime.strptime(line.last_changed, dt_format)
-    l2 = session.query(Line).get(line.id)
+
+def import_commune(cid, name):
+    c2 = session.query(Commune).get(cid)
+
+    if c2:
+        c = c2
+    else:
+        c = Commune()
+
+    if c:
+        c.id = cid
+        c.name = name
+        session.add(c)
+
+
+def import_station(info):
+    s = None
+    s2 = session.query(Station).get(info['\ufeff"HALTESTELLEN_ID"'])
+    last_changed = datetime.strptime(info['STAND'], dt_format)
+
+    if s2: 
+        if s2.last_changed < last_changed:
+            s = s2
+    else:
+        s = Station()
+        s.id = info['\ufeff"HALTESTELLEN_ID"']
+
+    if s:
+        s.name = info['NAME']
+        s.lat = float(info['WGS84_LAT'])
+        s.lon = float(info['WGS84_LON'])
+        s.last_changed = last_changed
+        s.commune_id = info['GEMEINDE_ID']
+        import_commune(info['GEMEINDE_ID'], info['GEMEINDE'])
+        session.add(s)
+
+
+def import_line(info):
+    l = None
+    l2 = session.query(Line).get(int(line['LINIEN_ID']))
+    last_changed = datetime.strptime(info['STAND'], dt_format)
 
     if l2:
-        if l2.last_changed >= last_changed:
-            continue
-        l = l2
+        if l2.last_changed < last_changed:
+            l = l2
     else:
         l = Line()
 
-    l.id = line.id
-    l.name = line.name
-    l.realtime = line.realtime
-    l.type = line.type
-    l.last_changed = datetime.strptime(line.last_changed, dt_format)
-    session.add(l)
+    if l:
+        l.id = int(info['LINIEN_ID'])
+        l.name = info['BEZEICHNUNG']
+        l.realtime = True if info['ECHTZEIT'] == '1' else False
+        l.type = info['VERKEHRSMITTEL']
+        l.last_changed = last_changed
+        session.add(l)
+
+
+with open(line_file, 'r') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    for line in reader:
+        import_line(line)
+
+
+with open(station_file, 'r') as f:
+    reader = csv.DictReader(f, delimiter=';')
+    for station in reader:
+        import_station(station)
+
 
 session.commit()
+session.flush()
