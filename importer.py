421ffaa426b8772728506648c4a4360a0fd96c4b
Florian Schweikert <kelvan@ist-total.org>
2013-11-06 19:59:16 +0100

    update lines feature

--- importer.py
+++ importer.py
@@ -5,7 +5,7 @@ from datetime import datetime
 from database import *
 import stations
 
-engine = create_engine('sqlite:///wl.db', echo=True)
+engine = create_engine('sqlite:///wl.db', echo=False)
 
 metadata = Base.metadata
 metadata.create_all(engine)
@@ -16,7 +16,16 @@ session = Session()
 dt_format = "%Y-%m-%d %H:%M:%S"
 
 for line in stations.Lines().values():
-    l = Line()
+    last_changed = datetime.strptime(line.last_changed, dt_format)
+    l2 = session.query(Line).get(line.id)
+
+    if l2:
+        if l2.last_changed >= last_changed:
+            continue
+        l = l2
+    else:
+        l = Line()
+
     l.id = line.id
     l.name = line.name
     l.realtime = line.realtime
