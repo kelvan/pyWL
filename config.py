cb7c71394a9aab23182930fffb71cd6c1187c0f8
Florian Schweikert <kelvan@ist-total.org>
2014-04-04 21:04:20 +0200

    accept production key if available
    fallback dev key

--- config.py
+++ config.py
@@ -1,11 +1,14 @@
 import os
 
-from pyWL.key import senderid_dev
+try:
+    from pyWL.key import senderid
+except ImportError:
+    from pyWL.key import senderid_dev as senderid
 
 proj_dir = os.path.dirname(__file__)
 
 baseurl = "http://www.wienerlinien.at/ogd_realtime"
-senderid= senderid_dev
+senderid= senderid
 
 datetimeformat = '%Y-%m-%dT%H:%M:%S.%f%z'
 
